62be0d5d01ae80be6bf5c9b264496a1f848c4a4d
Erk Ekin <erkekin@gmail.com>
2021-01-12 16:59:32 +0000

    new init implemented with array

--- Tests/BinaryTreeTests/BinaryTreeTests.swift
+++ Tests/BinaryTreeTests/BinaryTreeTests.swift
@@ -3,117 +3,103 @@ import BinaryTree
 
 final class BinaryTreeTests: XCTestCase {
   var tree: BinaryTree<Int>!
-  var initialItems: [Int]!
   var capturedItems: [Int]!
   
   override func setUp() {
     super.setUp()
     
-    tree = .empty
-    initialItems = [5, 5, 10, 15, 6]
-    initialItems.forEach{tree.insert($0)}
-    
+    tree = .init([5, 5, 10, 15, 6])
     capturedItems = []
   }
   
   override func tearDown() {
     tree = nil
-    initialItems = nil
     capturedItems = nil
     
     super.tearDown()
   }
-  
+
   func test_init() {
     XCTAssertEqual(tree, tree5_10_6_15)
   }
-  
+
   func test_min_no_left_tree() {
-    let tree = BinaryTree<Int>.empty.inserting(6).inserting(7).inserting(8).inserting(9).inserting(10)
+    let tree = BinaryTree<Int>([6, 7, 8 ,9 ,10])
     XCTAssertEqual(tree.min, tree)
   }
-  
+
   func test_min_no_right_tree() {
-    let tree = BinaryTree<Int>.empty.inserting(10).inserting(9).inserting(8).inserting(7).inserting(6)
+    let tree = BinaryTree<Int>([6, 7, 8 ,9 ,10].reversed())
     XCTAssertEqual(tree.min, .node(value: 6, .init(.empty, .empty)))
   }
-  
+
   func test_min_right_tree() {
     XCTAssertEqual(tree.children?.right.min, .node(value: 6, .init(.empty, .empty)))
   }
-  
+
   func test_remove_root_1_level() {
     var tree = BinaryTree<Int>.empty.inserting(1)
     tree.remove(1)
-    
+
     XCTAssertEqual(tree, .empty)
   }
-  
+
   func test_remove_one_childed() {
-    var actual = BinaryTree<Int>.empty
-    [16, 5, 10, 15, 6].forEach{actual.insert($0)}
-    
-    var expected = BinaryTree<Int>.empty
-    [16, 10, 15, 6].forEach{expected.insert($0)}
-    
+    var actual = BinaryTree<Int>([16, 5, 10, 15, 6])
+    let expected = BinaryTree<Int>([16, 10, 15, 6])
+
     actual.remove(5)
-    
+
     XCTAssertEqual(actual, expected)
   }
-  
+
   func test_remove_two_children_node() {
-    var actual = BinaryTree<Int>.empty
-    [16, 5, 10, 15, 3].forEach{actual.insert($0)}
-    
-    var expected = BinaryTree<Int>.empty
-    [16, 10, 3, 15].forEach{expected.insert($0)}
-    
+    var actual = BinaryTree<Int>([16, 5, 10, 15, 3])
+    let expected = BinaryTree<Int>([16, 10, 3, 15])
+
     actual.remove(5)
-    
+
     XCTAssertEqual(actual, expected)
   }
-  
+
   func test_remove_root_two_children_node() {
-    var actual = BinaryTree<Int>.empty
-    [16, 5, 10, 15, 3, 6, 20].forEach{actual.insert($0)}
-    
-    var expected = BinaryTree<Int>.empty
-    [20, 5, 10, 15, 3, 6].forEach{expected.insert($0)}
-    
+    var actual = BinaryTree<Int>([16, 5, 10, 15, 3, 6, 20])
+    let expected = BinaryTree<Int>([20, 5, 10, 15, 3, 6])
+
     actual.remove(16)
-    
+
     XCTAssertEqual(actual, expected)
   }
-  
+
   func test_removeTree_root_1_level() {
     var tree = BinaryTree<Int>.empty.inserting(1)
     tree.remove(treeUnder: 1)
     
     XCTAssertEqual(tree, .empty)
   }
-  
+
   func test_removeTree_root_3_level() {
     tree.remove(treeUnder: 5)
-    
+
     XCTAssertEqual(tree, .empty)
   }
-  
+
   func test_removeTree_leaf_3_level() {
     tree.remove(treeUnder: 15)
-    
-    XCTAssertEqual(tree, BinaryTree<Int>.empty.inserting(5).inserting(10).inserting(6))
+
+    XCTAssertEqual(tree, BinaryTree<Int>([5, 10, 6]))
   }
-  
+
   func test_removeTree_not_containing() {
     tree.remove(treeUnder: 1)
-    
+
     XCTAssertEqual(tree, tree)
   }
-  
+
   func test_removeTree_leaf_2_level() {
-    var tree = BinaryTree<Int>.empty.inserting(2).inserting(1)
+    var tree = BinaryTree<Int>([2, 1])
     tree.remove(treeUnder: 1)
-    
+
     XCTAssertEqual(tree, BinaryTree<Int>.empty.inserting(2))
   }
   
@@ -129,48 +115,48 @@ final class BinaryTreeTests: XCTestCase {
     tree.traverse(method: .inOrder) { [weak self] value, _ in
       self?.capturedItems.append(value)
     }
-    
+
     XCTAssertEqual(capturedItems, [5,6,10,15])
   }
-  
+
   func test_traversals_postOrder() {
     tree.traverse(method: .postOrder) { [weak self] value, _ in
       self?.capturedItems.append(value)
     }
-    
+
     XCTAssertEqual(capturedItems, [6,15,10,5])
   }
-  
+
   func test_height(){
     var tree: BinaryTree<Int> = .empty
     tree.insert(5)
-    
+
     XCTAssertEqual(tree.height, 1)
-    
+
     tree.insert(10)
-    
+
     XCTAssertEqual(tree.height, 2)
-    
+
     tree.insert(15)
-    
+
     XCTAssertEqual(tree.height, 3)
-    
+
     tree.insert(6)
-    
+
     XCTAssertEqual(tree.height, 3)
   }
-  
+
   func test_contains_bool(){
-    initialItems.forEach{
+    [5, 5, 10, 15, 6].forEach{
       XCTAssertTrue(tree.contains($0))
     }
   }
-  
+
   func test_search_path(){
     XCTAssertEqual(tree.search(6), [5,10,6])
     XCTAssertEqual(tree.search(1), [])
   }
-  
+
   func test_contains_other_tree(){
     XCTAssertTrue(tree.contains(tree5_10_6_15))
     XCTAssertTrue(tree5_10_6_15.contains(tree))
@@ -178,22 +164,17 @@ final class BinaryTreeTests: XCTestCase {
     XCTAssertTrue(tree.contains(.empty))
     XCTAssertTrue(BinaryTree<Int>.empty.contains(.empty))
   }
-  
+
   func test_string_preOrder(){
     var paths = [String]()
-    var tree: BinaryTree<String> = .empty
-    tree.insert("abc")
-    tree.insert("bca")
-    tree.insert("acb")
-    tree.insert("ab")
-    tree.insert("ca")
-    
+    let tree = BinaryTree<String>(["abc", "bca", "acb", "ab", "ca"])
+
     tree.traverse(method: .preOrder) { _, path in
       paths.append(path.joined(separator: "/"))
     }
-    
+
     XCTAssertEqual(tree.height, 3)
-    
+
     XCTAssertEqual(
       paths,
       [
@@ -205,7 +186,7 @@ final class BinaryTreeTests: XCTestCase {
       ]
     )
   }
-  
+
   private let tree5_10_6_15: BinaryTree<Int> = .node(
     value: 5,
     .init(
@@ -229,7 +210,7 @@ final class BinaryTreeTests: XCTestCase {
       )
     )
   )
-  
+
   private let tree10_6_15: BinaryTree<Int> = .node(
     value: 10, .init(
       .node(
@@ -247,7 +228,7 @@ final class BinaryTreeTests: XCTestCase {
       )
     )
   )
-  
+
   static var allTests = [
     ("test_init", test_init),
     ("test_traversals_preOrder", test_traversals_preOrder),
