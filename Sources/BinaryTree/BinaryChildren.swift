40982d0c80458771f4cbd3498c4c58a6aea618fe
Erk Ekin <erkekin@gmail.com>
2021-01-12 16:59:24 +0000

    contains added, hashable and equatable conformances implemented

--- Sources/BinaryTree/BinaryChildren.swift
+++ Sources/BinaryTree/BinaryChildren.swift
@@ -2,3 +2,6 @@ public struct BinaryChildren<T> {
   public let left: BinaryTree<T>
   public let right: BinaryTree<T>
 }
+
+extension BinaryChildren: Equatable where T: Equatable {}
+extension BinaryChildren: Hashable where T: Hashable {}
