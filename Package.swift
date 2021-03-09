f7d7d7b96ff94ae23460715cb5f309463ebffc16
Erk Ekin <erkekin@gmail.com>
2021-03-09 12:01:04 +0000

    swift packages fixed

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/swift-tree/BinaryTree.git", .exact("1.0.0"))
+    .package(url: "https://github.com/swift-tree/BinaryTree.git", .exact("1.0.0")),
   ],
   targets: [
     .target(
